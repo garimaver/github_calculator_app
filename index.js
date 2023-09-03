fd18e27cc115519ccff169c0057213a5d06b3ff2
garima verma <114860130+garimaver@users.noreply.github.com>
2023-09-03 22:23:16 +0530

    added js

---
+++ index.js
@@ -0,0 +1,60 @@
+(function() {
+
+let screen = document.querySelector('.screen');
+let buttons = document.querySelectorAll('.btn');
+let clear = document.querySelector('.btn-clear');
+let equal = document.querySelector('.btn-equal');
+
+
+buttons.forEach(function (button){
+       button.addEventListener('click', function(e){
+        let value = e.target.dataset.num;
+        screen.value += value;
+       })
+});
+
+    equal.addEventListener('click', function(e) {
+     if  (screen.value === '') {
+        screen.value = "Please enter";
+     }else  {
+        let answer = eval(screen.value);
+        screen.value = answer;
+     }
+})
+  
+clear.addEventListener('click', function(e){
+     screen.value = "";
+})
+
+
+})();
+
+/*let displayValue = "0";
+
+function appendToDisplay(value) {
+    if (displayValue === "0") {
+        displayValue = value;
+    } else {
+        displayValue += value;
+    }
+    updateDisplay();
+}
+
+function clearDisplay() {
+    displayValue = "0";
+    updateDisplay();
+}
+
+function calculate() {
+    try {
+        displayValue = eval(displayValue).toString();
+    } catch (error) {
+        displayValue = "Error";
+    }
+    updateDisplay();
+}
+
+function updateDisplay() {
+    const display = document.getElementById("result");
+    display.textContent = displayValue;
+}*/
